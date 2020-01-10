493f76cfc3bc932fa92507ba931fb8fcaaad0aa4
Divakar Murugesh <diva.srmvcas@gmail.com>
2020-01-10 12:30:36 +0530

    Initial Commit

--- guide/src/main/java/com/crispy/guide/Utils.kt
+++ guide/src/main/java/com/crispy/guide/Utils.kt
@@ -7,50 +7,54 @@ import android.net.Uri
 import android.util.Log
 
 
-fun Activity.canOpenUrl(eventLink: String) {
-    try {
-        if (eventLink.startsWith("sattva://")) {
-            if (appInstalledOrNot("com.meditation.tracker.android")) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
-            }
-        } else if (eventLink.startsWith("cosmicinsight://")) {
-            if (appInstalledOrNot("gman.vedicastro")) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
-            }
-        } else if (eventLink.startsWith("align27://")) {
-            if (appInstalledOrNot("com.dailyinsights")) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
-            }
-        } else if (eventLink.startsWith("japa108://")) {
-            if (appInstalledOrNot("com.gman.japa")) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
-            }
-        } else if (eventLink.startsWith("seven://")) {
-            if (appInstalledOrNot("aol.meditation.tracker.android")) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
-            }
-        } else if (eventLink.startsWith("prajnayoga://")) {
-            if (appInstalledOrNot("com.gmanlabs.prajnayoga")) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
-            }
-        } else {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
-        }
-    } catch (e: Exception) {
-        Log.e("crispy-utils error", e.message ?: "error")
-    }
-}
+object Utils {
 
-fun Activity.appInstalledOrNot(uri: String): Boolean {
-    try {
-        packageManager.getPackageInfo(uri, PackageManager.GET_ACTIVITIES)
-        return true
-    } catch (e: PackageManager.NameNotFoundException) {
+    fun Activity.canOpenUrl(eventLink: String) {
         try {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$uri")))
+            if (eventLink.startsWith("sattva://")) {
+                if (appInstalledOrNot("com.meditation.tracker.android")) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
+                }
+            } else if (eventLink.startsWith("cosmicinsight://")) {
+                if (appInstalledOrNot("gman.vedicastro")) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
+                }
+            } else if (eventLink.startsWith("align27://")) {
+                if (appInstalledOrNot("com.dailyinsights")) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
+                }
+            } else if (eventLink.startsWith("japa108://")) {
+                if (appInstalledOrNot("com.gman.japa")) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
+                }
+            } else if (eventLink.startsWith("seven://")) {
+                if (appInstalledOrNot("aol.meditation.tracker.android")) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
+                }
+            } else if (eventLink.startsWith("prajnayoga://")) {
+                if (appInstalledOrNot("com.gmanlabs.prajnayoga")) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
+                }
+            } else {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(eventLink)))
+            }
         } catch (e: Exception) {
             Log.e("crispy-utils error", e.message ?: "error")
         }
-        return false
     }
+
+    fun Activity.appInstalledOrNot(uri: String): Boolean {
+        try {
+            packageManager.getPackageInfo(uri, PackageManager.GET_ACTIVITIES)
+            return true
+        } catch (e: PackageManager.NameNotFoundException) {
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$uri")))
+            } catch (e: Exception) {
+                Log.e("crispy-utils error", e.message ?: "error")
+            }
+            return false
+        }
+    }
+
 }
